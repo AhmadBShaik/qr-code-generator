fde74e13408f361912195ed676da7e5944511282
AhmadBShaik <ahmad.shaik1106@gmail.com>
2022-01-07 04:40:33 -0500

    unit test the utils.js with jest

---
+++ tests/utils.test.js
@@ -0,0 +1,80 @@
+const urlify = require('./utils')
+
+const description = 'format string as url' 
+
+test(description,() => {
+    expect(
+        urlify(
+            "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source",
+            "Indiranagar 12th main"
+        )
+    ).toBe(
+        "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source=Indiranagar+12th+main"    
+    )
+})
+
+test(description,() => {
+    expect(
+        urlify(
+            "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source",
+            "Koramangala 12th main"
+        )
+    ).toBe(
+        "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source=Koramangala+12th+main"    
+    ) 
+})
+
+test(description,() => {
+    expect(
+        urlify(
+            "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source",
+            "Banshankari 6th main"
+        )
+    ).toBe(
+        "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source=Banshankari+6th+main"    
+    ) 
+})
+
+test(description,() => {
+    expect(
+        urlify(
+            "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source",
+            " Banshankari 6th main"
+        )
+    ).toBe(
+        "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source=Banshankari+6th+main"    
+    ) 
+})
+
+test(description,() => {
+    expect(
+        urlify(
+            "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source",
+            "Koramangala 12th main "
+        )
+    ).toBe(
+        "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source=Koramangala+12th+main"    
+    ) 
+})
+
+test(description,() => {
+    expect(
+        urlify(
+            "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source",
+            "Koramangala  12th  main"
+        )
+    ).toBe(
+        "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source=Koramangala+12th+main"    
+    ) 
+})
+
+test(description,() => {
+    expect(
+        urlify(
+            "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source",
+            "  Koramangala   12th   main  "
+        )
+    ).toBe(
+        "https://naveen.roringo.com/communities/yoga-with-yamini-thyroid-7?source=Koramangala+12th+main"    
+    ) 
+})
